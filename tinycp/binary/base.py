0551f0f551e825f080a4ff36d64a8fe604a9de72
Lucas Leão <heylucasleao@gmail.com>
2025-01-09 13:17:17 -0400

    ♻️ refactoring calibrate

--- tinycp/binary/base.py
+++ tinycp/binary/base.py
@@ -7,7 +7,7 @@ from sklearn.metrics import (
     log_loss,
     brier_score_loss,
     f1_score,
-    matthews_corrcoef,
+    balanced_accuracy_score,
 )
 import pandas as pd
 
@@ -106,7 +106,7 @@ class BaseConformalClassifier:
         p_prime, _ = self.calibration_layer.predict_proba(y_score)
         return p_prime
 
-    def calibrate(self, X, y):
+    def calibrate(self, X, y, minimal=0.2, func=balanced_accuracy_score):
         """
         Calibrates the alpha value to minimize the error rate
         using Cost Sensitive Learning methodology, using balanced_accuracy_score.
@@ -116,25 +116,21 @@ class BaseConformalClassifier:
             The test input samples.
         y: array-like of shape (n_samples,)
             The true labels for X.
+        minimal: float
+            The minimal value of alpha to consider.
 
-        For each alpha value (0.10, 0.09, …, 0.01), we do the following:
+        For each alpha value (0.01, 0.02, ..., minimal), we do the following:
         - Calculate predictions y_pred using the self.predict(X, alpha) function.
 
         Returns:
             The updated instance (self.alpha) with the calibrated alpha value.
         """
 
-        alphas = {
-            round(
-                k,
-                2,
-            ): None
-            for k in np.linspace(0.01, 0.10, 10)
-        }
+        alphas = {k: None for k in np.round(np.arange(0.01, minimal + 0.01, 0.01), 2)}
 
         for alpha in alphas:
             y_pred = self.predict(X, alpha)
-            alphas[alpha] = matthews_corrcoef(y, y_pred)
+            alphas[alpha] = func(y, y_pred)
 
         self.alpha = max(alphas, key=alphas.get)
 
@@ -158,6 +154,62 @@ class BaseConformalClassifier:
         y_pred = self.predict_set(X, alpha)
 
         return np.where(np.all(y_pred == [0, 1], axis=1), 1, 0)
+
+    def _expected_calibration_error(self, y, y_prob, M=5):
+        """
+        Generate the expected calibration error (ECE) of the classifier.
+
+        Parameters:
+        y: array-like of shape (n_samples,)
+            The true labels.
+        y_prob: array-like of shape (n_samples, n_classes)
+            The predicted probabilities.
+        M: int, default=5
+            The number of bins for the uniform binning approach.
+
+        Returns:
+        ece: float
+            The expected calibration error.
+
+        The function works as follows:
+        - It first creates M bins with uniform width over the interval [0, 1].
+        - For each sample, it computes the maximum predicted probability and makes a prediction.
+        - It then checks whether each prediction is correct or not.
+        - For each bin, it calculates the empirical probability of a sample falling into the bin.
+        - If the empirical probability is greater than 0, it computes the accuracy and average confidence of the bin.
+        - It then calculates the absolute difference between the accuracy and the average confidence, multiplies it by the empirical probability, and adds it to the total ECE.
+        """
+
+        # uniform binning approach with M number of bins
+        bin_boundaries = np.linspace(0, 1, M + 1)
+        bin_lowers = bin_boundaries[:-1]
+        bin_uppers = bin_boundaries[1:]
+
+        # get max probability per sample i
+        confidences = np.max(y_prob, axis=1)
+        # get predictions from confidences (positional in this case)
+        predicted_label = np.argmax(y_prob, axis=1)
+
+        # get a boolean list of correct/false predictions
+        predictions = predicted_label == y
+
+        ece = 0.0
+        for bin_lower, bin_upper in zip(bin_lowers, bin_uppers):
+            # determine if sample is in bin m (between bin lower & upper)
+            in_bin = np.logical_and(
+                confidences > bin_lower.item(), confidences <= bin_upper.item()
+            )
+            # can calculate the empirical probability of a sample falling into bin m: (|Bm|/n)
+            prob_in_bin = np.mean(in_bin)
+
+            if prob_in_bin > 0:
+                # get the accuracy of bin m: acc(Bm)
+                avg_pred = np.mean(predictions[in_bin])
+                # get the average confidence of bin m: conf(Bm)
+                avg_confidence_in_bin = np.mean(confidences[in_bin])
+                # calculate |acc(Bm) - conf(Bm)| * (|Bm|/n) for bin m and add to the total ECE
+                ece += np.abs(avg_pred - avg_confidence_in_bin) * prob_in_bin
+        return ece
 
     def evaluate(self, X, y, alpha=None):
         """
@@ -196,7 +248,7 @@ class BaseConformalClassifier:
         ece = rounded(self._expected_calibration_error(y, y_prob))
         empirical_coverage = rounded(self._empirical_coverage(X, alpha))
         generalization = rounded(self._evaluate_generalization(X, y, alpha))
-        matthews_corr = rounded(matthews_corrcoef(y, y_pred))
+        matthews_corr = rounded(balanced_accuracy_score(y, y_pred))
         f1 = rounded(f1_score(y, self.predict(X, alpha)))
 
         # Results aggregation
@@ -210,7 +262,7 @@ class BaseConformalClassifier:
             "ece": ece,
             "empirical_coverage": empirical_coverage,
             "generalization": generalization,
-            "matthews_corrcoef": matthews_corr,
+            "balanced_accuracy_score": matthews_corr,
             "f1_score": f1,
             "alpha": alpha,
         }
